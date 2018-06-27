b65b9b383e1f0974c04f839c1758ce4f775a5d67
nirkaufman <nirkaufman@gmail.com>
2018-06-27 15:32:56 -0400

    testing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import {Child} from "./Child";
-import './App.css';
 
 export class App extends React.Component {
-
-  state = { flag: true, counter: 1 };
-  input = React.createRef();
-
-  toggleChild(){
-    this.setState({ flag: !this.state.flag })
-  }
-
-  componentDidMount(){
-    this.state = { counter: 2 }
-    // this.input.current.focus();
-  }
-
   render(){
     return (
       <div>
         <h1>App Component</h1>
-        <input type="checkbox" onChange={() => this.toggleChild()}/>
-        <input type="text" ref={this.input}/>
-        { this.state.flag && <Child counter={this.state.counter}/> }
+        <h2>Just for test</h2>
       </div>
-    );
+    )
+  }
+}
+
+export class Link extends React.Component {
+  render() {
+    return <a href={this.props.url}>click</a>
   }
 }
